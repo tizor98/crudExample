3050ecc36404c17ea332dedb25a6a60b4388a111
Alberto Ortiz <baortiz98@outlook.com>
2022-11-02 17:51:30 -0500

    Multer library added to handle files

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,11 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 
+// Ejecutando lo necesario para subir archivos con Multer
+const multer = require("multer");
+
+const storage = multer.diskStorage( { // En qué carpeta se guardarán los archivos y cómo se nombrarán
+   destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, "../public/images"));
+   },
+   filename: (req, file, cb) => {
+      const newFileName = Date.now() + "_img_" + path.extname(file.originalname);
+      cb(null, newFileName);
+   }
+})
+
+const upload = multer({storage: storage}) // establece la configuración para guardar archivos, se usa en los get
+
+// Guardando la ubicación de los datos
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
+// Controladores
 const controller = {
 	// Root - Show all products
 	products: (req, res) => {
